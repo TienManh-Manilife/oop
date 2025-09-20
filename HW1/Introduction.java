8bcf83555ff2243fc117268712eed2baf4c4aee5
Nguyen Tien Manh <manilife217@gmail.com>
2025-09-21 00:39:02 +0700

    Hoan thanh HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,18 @@
+public class Introduction {
+
+    /**
+     * In ra thong tin
+     * In ra 20 dong
+     */
+    public static void main(String[] args) {
+        System.out.println("NGUYEN TIEN MANH" + '\t' + "24020220" + '\t' + "K69I-IT4" + '\t' + "TienManh-Manilife" + '\t' + "manilife217@gmail.com");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("...");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
